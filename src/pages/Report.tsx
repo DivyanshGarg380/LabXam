5ceac042624818b8462b0f23c3fb870be8d79384
Divyansh Garg <HIMYNAMEISDIVYANSH@GMAIL.COM>
2026-03-14 19:11:30 +0530

    Reports blocked and added the necessary Notice

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -77,7 +77,7 @@ export default function Report() {
 
         <div className="flex flex-col gap-3">
             <Button className="w-full h-11" disabled onClick={handleSubmit}>
-                Send Report
+                Feature coming soon!
             </Button>
 
             <Button
